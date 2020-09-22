529fbbfa8b13ce0935077a56035df584b8dec215
jiajiwei <jiajiwei@jiujiutech.cn>
2020-09-22 18:40:18 +0800

    [feat] update readme

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
             host: 'localhost', // Localhost (default: none)
             port: 8545, // Standard Ethereum port (default: none)
             network_id: '*', // Any network (default: none)
-            gas: 10000000,
+            gas: 6721975,
         },
         coverage: {
             host: 'localhost',
